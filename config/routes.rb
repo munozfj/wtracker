2ce19f0880362fb0ae577ed80c419d4154064afc
Fernando Muñoz <munozfj@hotmail.com>
2014-03-04 17:00:01 +0100

    New static pages

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Wtracker::Application.routes.draw do
   
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
