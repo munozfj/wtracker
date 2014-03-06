ad4e20dfc7f0d68d6a4eed1971599ab12776799a
Fernando Muñoz <munozfj@hotmail.com>
2014-03-06 14:32:27 +0100

    Incorporación de Devise - Parte 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,6 @@
 class User < ActiveRecord::Base
-
-  before_save {self.email = email.downcase }
-
-  has_secure_password
-
-  #Validaciones Name
-  validates :name, presence: true,
-                   length: { maximum: 100 }
-
-  #Validaciones Email
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true,
-                    format: { with: VALID_EMAIL_REGEX},
-                    uniqueness: {casesensitive: false}
-
-  #Validaciones Password_digest
-  validates :password, presence: true,
-                                 length: {minimum: 6},
-                                 confirmation: true;
-
-  #Validaciones Password_disgest_confirmation
-  validates :password_confirmation, presence: true;
-
-
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
