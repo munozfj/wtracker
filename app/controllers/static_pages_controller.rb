3708a4d4a8ff611f25775b17aac9ae30d2a6ca78
Fernando Muñoz <munozfj@hotmail.com>
2014-03-14 16:14:48 +0100

    Modificaciones a los graficos

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -23,5 +23,17 @@ class StaticPagesController < ApplicationController
   end
 
   def graph
+    @weight_max=current_user.weights.maximum("weight")
+    @weight_min=current_user.weights.minimum("weight")
+    @weight_max+=0.1
+    @weight_min-=0.1
+
+    #fat%
+    @fat_max=current_user.weights.maximum("fat_porcentage")
+    @fat_min=current_user.weights.minimum("fat_porcentage")
+    @fat_max+=0.1
+    @fat_min-=0.1
+    #puts @fat_max
+    #puts @fat_min
   end
 end
